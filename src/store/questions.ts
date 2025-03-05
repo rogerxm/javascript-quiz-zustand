cb97e872a1e015b2e928cdc812fb5df4713a0467
Roger Montes <rogerxmontes@gmail.com>
2025-03-04 22:03:23 -0600

    fix fetch de la data

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -20,7 +20,7 @@ export const useQuestionsStore = create<State>()(
         questions: [],
         currentQuestion: 0,
         fetchQuestions: async (limit: number) => {
-          const res = await fetch("http://localhost:5173/data.json");
+          const res = await fetch("/data.json");
           const json = await res.json();
 
           const questions = json
